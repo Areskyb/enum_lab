ca2aa2787a7eb05b04498168f4050484834aa234
Aresky Berkane <areskyberkane@gmail.com>
2020-01-09 13:53:57 +0000

    add test for get Cards

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,15 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+
+    int getCards() {
+        return this.cards.size();
+    }
 }
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckTest {
+    Deck deck;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasEmptyDeck(){
+        assertEquals(0, deck.getCards());
+    }
+
+
+
+
+}
+
